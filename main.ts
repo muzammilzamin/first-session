b9ff5b11bb43153f8e9fe0589c18bef0b7e3f504
muzammilzamin <159897882+muzammilzamin@users.noreply.github.com>
2024-03-26 03:42:57 +0500

    this is my second project

---
+++ main.ts
@@ -0,0 +1,39 @@
+// we use let,var,const
+//var ko use nhi karengy gy ham ok.
+
+//let me banaye gay variable ko change kr sakty hain
+//variable k naam k shoro me ham koi number nhi de sakty or reserved name bhi nhi rakh sakty 
+//EX = const,console or koi special character shoro me bhi nhi laga sakty 
+
+// let message = "hello world";
+// message = "hello chachi"
+// console.log(message);
+
+//const mey banye gai variable ko change nhi kr sakty ham.
+
+// const message = "hello world"
+// console.log(message)
+
+// boolean mey "" nhi lagaty hain or is me ham sirf 2 hi cheezy likh sakty hain true or false.
+// let message = "Muzammil";
+// let age = 22; 
+// let isMarried = false;
+// console.log(isMarried);
+// console.log(age);
+// console.log(message);
+
+//concortination
+// let num1 = "1";
+// let num2 = "1";
+// console.log(num1 + num2)
+// jab ham variables ko plus karengy to wo add nhi hoga balk 1 or 1 .
+//  11 higa na k 2.q k ye string me hain agr boolean mey hoty to + ho jaty
+
+// NEECHY DI GAI EXAMPLE BOOLEAN KI HAI IS ME 10 OR 5 = 15 HONGY Q K YE BOLEAN ME 
+// BOOLEAN KI PEHCHAN YE HAI K OS ME "" NHI LAGTY NUMBERS MEY
+
+
+// let num1 = 10
+// let num2 = 5
+// console.log(num1+num2)
+
